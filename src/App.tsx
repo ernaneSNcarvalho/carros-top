097c9beb6ad3932c8ab7b902d31ee7dca5e97282
ernane <ernanecarvalho89@gmail.com>
2022-01-31 10:08:13 -0300

    Bootstrap theme customization

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,11 @@
 import React from 'react';
-
+import './assets/styles/custom.scss';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <h1>CARROS TOP</h1>
+      <h1 className="bg-success text-warning">CARROS TOP</h1>
     </div>
   );
 }
